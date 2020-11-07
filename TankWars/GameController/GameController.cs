5280b2284acae13cb708cbce90295c85edbe71f9
Andrew Porter <porter@eng.utah.edu>
2020-11-07 13:47:05 -0700

    Started on controller class to update the view

---
+++ TankWars/GameController/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+
+namespace TankWars
+{
+    public class GameController
+    {
+
+        public delegate void EventHandler(IEnumerable<string> JsonMessage);
+        public event EventHandler updateWorld;
+
+        public delegate void ConnectedHandler();
+        public event ConnectedHandler Connected;
+
+        public delegate void ErrorHandler(string err);
+        public event ErrorHandler Error;
+
+        // State representing the connection with the server
+        SocketState theServer = null;
+
+        // Connect to server
+        // start event loop with socket state
+
+    }
+}
